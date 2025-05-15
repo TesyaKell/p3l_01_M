509ade9df3db726801952f192b9b58b5eadd3a32
Imanuella Daniel <220711901@students.uajy.ac.id>
2025-05-15 12:29:37 +0700

    organisasi

--- app/Filament/Resources/OrganisasiResource/Pages/ListOrganisasis.php
+++ app/Filament/Resources/OrganisasiResource/Pages/ListOrganisasis.php
@@ -13,7 +13,7 @@ class ListOrganisasis extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+
         ];
     }
 }
