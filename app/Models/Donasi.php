94f1a7ebae534179708c0071b07e4287d69f70a6
Imanuella Daniel <220711901@students.uajy.ac.id>
2025-05-15 12:16:46 +0700

    FIXXX

--- app/Models/Donasi.php
+++ app/Models/Donasi.php
@@ -31,11 +31,11 @@ class Donasi extends Model
         return $this->belongsTo(RequestDonasi::class, 'id_request');
     }
 
-    /*
+
     public function penitip()
     {
         return $this->belongsTo(Penitip::class, 'id_penitip', 'id_penitip'); // Ensure the relationship is correct
     }
-    */
+
 
 }
