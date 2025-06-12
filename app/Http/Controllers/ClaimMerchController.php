ec0f0642762b0c8d680e40fdbc336eaf4553a114
Imanuella Daniel <220711901@students.uajy.ac.id>
2025-06-12 18:13:46 +0700

    perbaiki

--- app/Http/Controllers/ClaimMerchController.php
+++ app/Http/Controllers/ClaimMerchController.php
@@ -8,13 +8,31 @@ use App\Models\Merchandise;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 
+use function compact;
+
 class ClaimMerchController extends Controller
 {
+    // public function index()
+    // {
+    //     $claimMerchList = ClaimMerch::with(['merchandise', 'pembeli'])->get();
+    //     return view('claimMerc', compact('claimMerchList'));
+    // }
+
+
     public function index()
     {
-        $claimMerchList = ClaimMerch::with(['merchandise', 'pembeli'])->get();
-        return view('claimMerc', compact('claimMerchList'));
+        // Ambil data ClaimMerch bulan Juni yang merchandise-nya punya poin > 100
+        $claimMerchList = ClaimMerch::with(['merchandise', 'pembeli'])
+            ->whereMonth('tanggal_request', 6)
+            ->whereHas('merchandise', function ($query) {
+                $query->where('poin', '>', 100);
+            })
+            ->get();
+
+
+        return view('claimMerc', compact('claimMerchList', 'poin'));
     }
+
 
     public function selesaikan(Request $request, $id)
     {
