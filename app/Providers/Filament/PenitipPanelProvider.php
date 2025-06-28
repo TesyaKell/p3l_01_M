f1829ed9da1ed7412fa54c11095ae33a6dae010c
Tesya Rakhel <220711896@students.uajy.ac.id>
2025-06-28 11:02:32 +0700

    coba fix deployan gabisa upload gambar

--- app/Providers/Filament/PenitipPanelProvider.php
+++ app/Providers/Filament/PenitipPanelProvider.php
@@ -13,7 +13,6 @@ use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
@@ -55,7 +54,6 @@ class PenitipPanelProvider extends PanelProvider
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
