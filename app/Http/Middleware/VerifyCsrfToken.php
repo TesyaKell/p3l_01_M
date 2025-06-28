85b55ba5e2f36380724191d9fa074db97a906531
Imanuella Daniel <220711901@students.uajy.ac.id>
2025-06-28 11:15:44 +0700

    coba upload

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -5,19 +5,16 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
-class VerifyCsrfToken
+class VerifyCsrfToken extends Middleware
 {
     protected $except = [
         '*livewire/upload-file',
     ];
 
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+
+    public function handle($request, Closure $next)
     {
         return $next($request);
     }
